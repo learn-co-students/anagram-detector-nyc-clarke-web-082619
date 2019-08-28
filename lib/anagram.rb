91de2f560e33586bbbe98bd9e75f84e399b91bb8
github username <github email address>
2019-08-28 16:43:31 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+
+class Anagram
+
+
+    def initialize(string)
+        @string = string
+    end
+
+    def match(words)
+        array = []
+        words.each do |word|
+            if word.chars.sort(&:casecmp) == @string.chars.sort(&:casecmp)
+                array << word
+            end
+        end
+        array
+    end
+end
+
