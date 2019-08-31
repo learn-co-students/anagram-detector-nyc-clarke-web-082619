da35b37c1728f2f3dd91a463c20ce08c1f5b0b48
github username <github email address>
2019-08-31 12:40:06 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
-# Your code goes here!
+class Anagram 
+attr_accessor :word 
+
+
+    def initialize(word)
+        @word = word
+    end 
+
+    def match(anagram_array) #returns all matches in an array 
+        match_ary = [] 
+        anagram_array.each do |new_word| 
+            if new_word.split("").sort == @word.split("").sort
+                match_ary << new_word
+            end 
+        end 
+        return match_ary
+    end 
+
+end 
+
+# anagram_array = ["hello", "world", "zombies", "pants", "dipper"]
+# word = diaper 
